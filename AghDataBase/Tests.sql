11808e8be33e1ac256ad8edf44ca06021982ffd6
Mateusz <mateusz.popielarz@fortedigital.no>
2019-04-02 19:31:27 +0200

    One stored procedure pew firle
    Added procedure for adding an address

---
+++ AghDataBase/Tests.sql
@@ -0,0 +1,7 @@
+﻿EXEC AddAddress 'street', 5,5,'38-400','City', 'Province', 'Country';
+
+SELECT * from Countries
+SELECT * from Provinces
+SELECT * from Cities
+SELECT * from Streets
+SELECT * from Buildings
