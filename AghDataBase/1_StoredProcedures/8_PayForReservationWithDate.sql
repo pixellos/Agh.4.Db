9411342776b70f97c095099986797e1388335ee0
Mateusz <mateusz.popielarz@fortedigital.no>
2019-04-13 17:04:17 +0200

    PayForReservation wo tests

--- AghDataBase/1_StoredProcedures/8_PayForReservationWithDate.sql
+++ AghDataBase/1_StoredProcedures/8_PayForReservationWithDate.sql
@@ -1,9 +1,14 @@
 ﻿/*
 	Założenie: Klient w treści przelewu wpisuje numer konferencji, gdy przelew był zły dzwonimy do klienta i 
+	Założenie: Gdy rezerwacja nie istnieje rzucamy wyjątek 'RESERVATION DOES NOT EXISTS' o kluczu 1- oznacza, że płatność ma zostać zwrócona klientowi
+	Założenie: Płacimy zawsze za całość konferencji 
+	Założenie: Gdy płatność już istnieje rzucamy wyjątek 'RESERVATION HAS BEEN ALREADY PAID FOR' o kluczu 2
+	Założenie:Gdy klient zapłaci za dużo/za mało 'RESERVATION HAS BEEN ALREADY PAID FOR' o kluczu 2
 
 	Klient wpłacił płatność w dniu podanym
 	W przypadku odrzucenia rzucany jest wyjątek
 
+	ZAŁOŻENIE - 
 */
 CREATE OR ALTER PROCEDURE PayForReservationWithADate
 	@PersonalNumber varchar(50),
@@ -14,12 +19,38 @@ AS
 
 DECLARE @clientId INT;
 SELECT @clientId = dbo.GetIndividualClientOrThrow(@PersonalNumber);
-	
-	DECLARE @conferenceDate DATETIME;
 
-	SELECT @conferenceDate = Date FROM dbo.ConferenceDays WHERE ConferenceId = @ConferenceId ORDER BY Date;
+DECLARE @price decimal;
+select @price = dbo.GetConferencePrice(@ConferenceId, @PersonalNumber, @PaymentDate);
 
-	SELECT * FROM dbo.ConferencePrices WHERE @ConferenceId = ConferenceId AND  DATEADD(day, TillConferenceStart, @conferenceDate) > @PaymentDate;
 
-	RETURN 0
+DECLARE @priceId int;
+select @priceId = dbo.GetConferencePriceId(@ConferenceId, @PersonalNumber, @PaymentDate);
+
+DECLARE @reservationId int;
+SELECT @reservationId = Id FROM Reservations WHERE ConferenceId = @ConferenceId and ClientId = @clientId;
+
+if @reservationId = null 
+BEGIN
+	;THROW 51001, 'RESERVATION DOES NOT EXISTS', 1;
+END
+
+DECLARE @reservationPaymentId int;
+SELECT @reservationPaymentId = Id FROM ReservationPayments 
+WHERE Id = @reservationId;
+
+IF @reservationPaymentId != null
+BEGIN
+	;THROW 51002, 'RESERVATION HAS BEEN ALREADY PAID FOR', 2;
+END
+
+IF @price != @Ammount
+BEGIN
+	DECLARE @msg NVARCHAR(2048) = 'RESERVATION CANNOT BE PLACED - AMMOUNT DIFFERS WITH PRICE: ' + @price;   
+	THROW 51003, @msg, 3;
+END	
+
+INSERT INTO ReservationPayments(Id, ClientId, Ammount, ConferencePricesId) VALUES(@reservationId ,@clientId, @price, @priceId)
+RETURN @reservationId;
+
 GO
