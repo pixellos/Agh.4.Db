2640dea1d5f10fa97a519aaf23dd23a4dc8655af
Mateusz <mateusz.popielarz@fortedigital.no>
2019-05-16 23:47:06 +0200

    Workshpo Triggers

--- AghDataBase/5_Triggers.sql
+++ AghDataBase/5_Triggers.sql
@@ -1,1 +1,19 @@
-﻿
+﻿CREATE TRIGGER DISALLOW_WORKSHOP_RESERVATION_FOR_SAME_CLIENT
+ON [WorkshopReservations]
+AFTER INSERT
+AS
+
+IF EXISTS (
+SELECT * FROM inserted i INNER JOIN Workshops WI ON i.Id = WI.Id where 
+	(SELECT COUNT(*) FROM WorkshopReservations INNER JOIN Workshops WRW ON WorkshopReservations.Id = WRW.Id 
+		WHERE  WI.StartTime <= WRW.StartTime AND WI.EndTime <= WRW.EndTime AND WI.EndTime >= WRW.StartTime
+	)
+	> 0
+)
+BEGIN
+RAISERROR ('Same client can not have overlapping time.', 16, 1);
+ROLLBACK TRANSACTION;
+RETURN 
+END;
+
+GO
